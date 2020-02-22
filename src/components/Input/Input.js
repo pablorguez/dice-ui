1341f3ef659095fa617c79cc29f285da10919acd
Pablo Rodriguez <pablo.rcampos@gmail.com>
2020-02-22 12:27:08 +0100

    WIP List and Link components

---
+++ src/components/Input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { system, color, css } from 'styled-system';
+import css from '@styled-system/css';
+
+const StyledInput = styled('input')(css({}));
+
+const Input = () => <StyledInput />;
+
+Input.propTypes = {};
+
+Input.defaultProps = {};
+
+export default Input;
